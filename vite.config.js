7ebdcd95727ccd9e1295ffc6705e30b0896fecb8
Tim Lin <e0610k@gmail.com>
2023-01-04 01:25:03 +0800

    fix(vite.config): 修改gh-pages path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === 'production' ? '/vue-sidebar-practice-v1/' : '/',
   plugins: [
     vue({
       template: {
